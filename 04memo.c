8cef6735ee3ad5f34fbc87a7c3444d335411d147
gurd <justchatting1213@gmail.com>
2026-07-14 10:55:00 +0900

    임시

--- 04memo.c
+++ 04memo.c
@@ -1,7 +1,5 @@
 동작하는 block마다의 시작주소는 bp로 base address다. 각 블록의 시작 주소로 고정된 값인데, 스택은 아래로 자라나므로 "가장 최근 동작한 주소" 인 sp는 스택이 자람에따라 점점 주소가 아래쪽으로 잡히게 된다는것도 상상 가능하다.
 
-스택이 자라난다는게 무슨 의미일까 
-
 __asm__
   mov ah, 0x0e ;; tty mode
   mov bp, 0x8000 ;; 0x7c00에서 대충 멀리 떨어진 주소로 아무거나 적은거다. overwrite를 피하기위함
@@ -12,9 +10,22 @@ __asm__
   push 'C'
 
   mov al, [0x7ffe] ;; 0x8000 - 2
-  int 0x10
+  int 0x10 ;; 스택이 자라난다는 말의 의미를 알아보자
 
   mov al, [0x8000]
   int 0x10
 
-  
+  pop bx
+  mov al, bl
+  int 0x10 ;; printf C
+
+  pop bx
+  mov al, bl
+  int 0x10 ;; printf B
+
+  pop bx
+  mov al, bl
+  int 0x10 ;; printf A
+
+  mov al, [0x8000]
+  int 0x10 ;; 스택에서 한번 pop 된건 현재 해당 스택에 쓰레기값이 존재한다는 근거가 된다.
